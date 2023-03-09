54ed6d1ccca9ec44243bfba49a2507f2a299a746
pdavisDU <pdavisdu@gmail.com>
2023-03-09 12:49:13 -0600

    split expenseitem and expensedate components into two seperate components. Added CSS for the new expensedate component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 // here where I passed the word props as an argument, since this is our own function we can name this argument whatever we want. Typically it is named props
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
