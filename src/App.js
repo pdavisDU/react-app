0bf1e3d7e29b73fe78b65805639dcc63210a90a4
pdavisDU <pdavisdu@gmail.com>
2023-03-15 10:48:02 -0500

    forgot to add .items to props.items in expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -30,27 +31,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* Here where we have the words title and amount, we can choose whatever words we want for that on the outside of the brackets */}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,29 +1,32 @@
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
   return (
     <div>
       {/* Here where we have the words title and amount, we can choose whatever words we want for that on the outside of the brackets */}
       <ExpenseItem
-        title={props[0].title}
-        amount={props[0].amount}
-        date={props[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={props[1].title}
-        amount={props[1].amount}
-        date={props[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={props[2].title}
-        amount={props[2].amount}
-        date={props[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
       <ExpenseItem
-        title={props[3].title}
-        amount={props[3].amount}
-        date={props[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
       />
     </div>
   );
 }
+
+export default Expenses;
