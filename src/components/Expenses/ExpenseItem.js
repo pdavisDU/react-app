c8e5fd1e8712915a13d030845e21ca3367b06821
pdavisDU <pdavisdu@gmail.com>
2023-03-21 11:13:58 -0500

    adding useState to change state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 // here where I passed the word props as an argument, since this is our own function we can name this argument whatever we want. Typically it is named props
 function ExpenseItem(props) {
+  // useState needs a value
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler() {
+    setTitle("updated");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
