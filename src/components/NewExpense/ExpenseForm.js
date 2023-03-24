3590eaf0eb12d287d564f9b160fcfb0d625099ae
pdavisDU <pdavisdu@gmail.com>
2023-03-24 11:43:55 -0500

    added value to the inputs to clear the fields upon submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -48,13 +48,20 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTItle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -62,6 +69,7 @@ const ExpenseForm = () => {
             type="number"
             min="0,01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -71,6 +79,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
